9961f9d2c026a4bec0dfffcae1633f480f33efd2
payalamolina <51799770+payalamolina@users.noreply.github.com>
2020-06-02 13:31:29 -0600

    Index y ejercicio 2
    Primera carga con el index y el ejercicio 2

---
+++ Ejercicio2.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<h1>Ejercicio 2</h1>
+
+<?php
+
+    //Este es un comentario de una linea
+
+    /*
+        Este es 
+        Un comentario de multiples
+        lineas
+    */
+    $NombreClase = "Lenguaje 4";
+
+    echo "<html><head><meta charset=\"utf-8\"></head>";
+    echo "<body>";
+    echo "<h2>Esta es la clase de $NombreClase mañana y tarde (Simbolo) </h2>";
+    echo "<h2>Esta es otra fila de la clase de ".$NombreClase." con dos cadenas conectadas </h2>";
+    echo "</body>";
+    echo "</html>";
+
+    $numero = 50;
+    $calculo = $numero / 2 + 6;
+    echo "El resultado de la operacion es : $calculo";
+
+    $logico = true;
+    $logico2 = false;
+    echo "<p>El contenido de una variable tipo booleana verdadero: $logico y variable booleana falsa : $logico2</p>";
+
+    $variableNombreClase = "NombreClase";
+    echo "<p>Este es un nombre de variable extraido de una cadena ".$$varibleNombreClase."</p>";
+?>
+
+</body>
+</html>
+
---
+++ Index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+    <h1>Ejercicios PHP</h1>
+
+    <ul>
+        <li><a href="Ejercicio2.php" target="_new">Ejercicio 2</a></li>
+    </ul>
+
+</body>
+</html>
