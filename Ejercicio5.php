ed863040a06851f79bd7aa063cdaeeb0f6ab14c3
payalamolina <51799770+payalamolina@users.noreply.github.com>
2020-06-02 15:15:41 -0600

    errores en ejercicio 5

--- Ejercicio5.php
+++ Ejercicio5.php
@@ -8,31 +8,31 @@
 
     header ("Content-type: text/html;charset=\"utf-8\"");
 
-    for ($i=1; $i<=10; $i++)
+    for ($i=1;$i<=10;$i++)
     {
         echo "<p>$i</p>";
     }
     echo "<p>Finaliza ciclo for</p>";
 
-    for ($i=10; $i>=0; $i--)
+    for ($i=10;$i>=0;$i--)
     {
         echo "<P>Finaliza ciclo for</p>";
     }
 
     $familia = array("Pablo", "Malenis", "Jonathan", "Melani");
 
-    for ($i=0; $i<=sizeof($familia);$i++)
+    for ($i=0;$i<=sizeof($familia);$i++)
     {
         echo "<h2>$familia[$i] numero de elementos $i </h2>";
         echo "<br>"; 
     }
 
-    $x=1;
+    /*$x=1;
     while ($x <= 10)
     {
         echo "El elemento es : $x <br>";
         $x++
-    }
+    }*/
 
 ?>
 
